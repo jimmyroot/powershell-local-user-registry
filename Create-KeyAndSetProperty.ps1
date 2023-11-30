479c15443755ba042462358186fd72c1e60d92b5
jimmyroot <jimmybnewman@gmail.com>
2023-11-30 14:23:38 +0000

    First working version
    Powershell script to inject registry keys into the currently logged on user registry hives

---
+++ Create-KeyAndSetProperty.ps1
@@ -0,0 +1,93 @@
+$regexSID = 'S-1-[0-59]-\d{2}-\d{8,10}-\d{8,10}-\d{8,10}-[1-9]\d{4}$'
+#$keyToHideMSStorage = 'Software\Policies\Microsoft\Office\16.0\Common\Internet'
+##$keyToSetDefaultSaveLocationWord = 'Software\Microsoft\Office\16.0\Word\Options'
+#$keyToSetDefaultSaveLocationWord = 'Software\Microsoft\Office\16.0\Excel\Options'
+#$keyToSetDefaultSaveLocationWord = 'Software\Microsoft\Office\16.0\PowerPoint\RecentFolderList'
+
+$properties = [System.Collections.ArrayList]@(
+    @{
+        'key' = 'Software\Policies\Microsoft\Office\16.0\Common\Internet'
+        'name' = 'OnlineStorage'
+        'type' = 'Dword'
+        'value' = 3
+    },
+    @{
+        'key' = 'Software\Microsoft\Office\16.0\Word\Options'
+        'name' = 'DOC-PATH'
+        'type' = 'ExpandString'
+        'value' = 'P:\'
+    },
+    @{
+        'key' = 'Software\Microsoft\Office\16.0\Excel\Options'
+        'name' = 'DefaultPath'
+        'type' = 'ExpandString'
+        'value' = 'P:\'
+    },
+    @{
+        'key' = 'Software\Microsoft\Office\16.0\PowerPoint\RecentFolderList'
+        'name' = 'Default'
+        'type' = 'ExpandString'
+        'value' = 'P:\'
+    }
+)
+
+function Get-PathToKey {
+    [CmdletBinding()]
+    param(
+        [Parameter()]
+        [Microsoft.Win32.RegistryKey]$UserProfileInHKU,
+        [Parameter()]
+        [System.string]$PathToKey
+    )
+
+    return Join-Path $userProfileInHKU.ToString().Replace('HKEY_USERS', 'HKU:') $PathToKey
+}
+
+function Create-KeyAndSetProperty {
+    [CmdletBinding()]
+    param(
+        [Parameter()]
+        [System.string]$Key,
+        [Parameter()]
+        [System.string]$Type,
+        [Parameter()]
+        [System.string]$Name,
+        [Parameter()]
+        [System.string]$Value
+    )
+
+    $targetKey = try {
+        Get-Item $Key -ErrorAction Stop
+    }
+    catch {
+        New-Item $Key
+    }
+
+    try {
+        New-ItemProperty -Path $targetKey.PSPath -Name $Name -Type $Type -Value $Value -ErrorAction Stop
+    }
+    catch {
+        Set-ItemProperty -Path $targetKey.PSPath -Name $Name -Type $Type -Value $Value
+    }
+}
+
+function Set-RegKeys {
+    [CmdletBinding()]
+    param(
+        [Parameter()]
+        [Microsoft.Win32.RegistryKey]$Profiles
+    )
+    ForEach ($Profile in $Profiles) {
+
+        ForEach ($p in $properties) {
+            $fullPathToKey = Get-PathToKey -UserProfileInHKU $Profile -PathToKey $p.key
+            Create-KeyAndSetProperty -Key $fullPathToKey -Name $p.name -Type $p.type -Value $p.value
+        }
+    }
+}
+
+New-PSDrive HKU Registry HKEY_USERS
+
+$loggedInUserProfiles = Get-ChildItem HKU: -ErrorACtion SilentlyContinue | Where-Object {$_.Name -match $regexSID}
+
+Set-RegKeys -Profiles $loggedInUserProfiles
